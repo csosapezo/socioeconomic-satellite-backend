d0df2729a96d4ba68a8b2dbdacadbfe145ca500c
Carlos Sosa <c.sosa@pucp.edu.pe>
2021-05-01 00:32:42 -0500

    add income level parser

---
+++ config/income_levels.py
@@ -0,0 +1,18 @@
+import os
+
+import configparser
+
+config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "init_files", "levels.ini"))
+
+
+class IncomeLevels:
+    def __init__(self, ini_file=config_file, scale='LIMA'):
+        config = configparser.ConfigParser()
+        config.read(ini_file)
+        self.levels = {}
+
+        for index, level in config[scale].items():
+            self.levels[level] = int(index)
+
+    def __call__(self, level):
+        return self.levels[level]
